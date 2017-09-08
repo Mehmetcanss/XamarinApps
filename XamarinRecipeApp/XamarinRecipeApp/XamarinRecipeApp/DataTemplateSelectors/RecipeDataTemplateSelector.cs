601d348eb0c4d5f47411f7dab267a48dcbf1690e
Mehmetcan <mehmetcan.sinir@live.com>
2017-09-08 20:59:36 +0200

    added template selectors and different templates

---
+++ XamarinRecipeApp/XamarinRecipeApp/XamarinRecipeApp/DataTemplateSelectors/RecipeDataTemplateSelector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XamarinRecipeApp.Models;
+using XamarinRecipeApp.ViewCells;
+
+namespace XamarinRecipeApp.DataTemplateSelectors
+{
+    public class RecipeDataTemplateSelector : DataTemplateSelector
+    {
+
+        DataTemplate recipeTemplate;
+        DataTemplate recommendedTemplate;
+
+        public RecipeDataTemplateSelector()
+        {
+            recipeTemplate = new DataTemplate(typeof(RecipeCell));
+            recommendedTemplate = new DataTemplate(typeof(RecommendedRecipeCell));
+        }
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var recipe = item as Recipe;
+            if (recipe == null) return null;
+            return recipe.Recommended ? recommendedTemplate : recipeTemplate;
+                
+        }
+    }
+}
